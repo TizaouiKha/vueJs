27530c5b4c52a530c13eab3b181f4feac6aad185
khalnetLaMoula <khalnet92t@gmail.com>
2026-06-01 12:04:29 +0200

    tp1 - Vue.js

---
+++ tp1/tp1/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  const message = "Hello World!";
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+
+  <p>{{message}}</p>
+  <p>{{'2 + 2 = ' + (2 + 2)}}</p>
+</template>
+
+<style scoped></style>
